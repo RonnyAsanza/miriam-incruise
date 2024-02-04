fa3578622d3beda50ff467b4200b78fb22900c62
RonnyAsanza <ronnyasanza@gmail.com>
2024-02-05 00:24:40 +0100

    configuraciones

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,8 +8,11 @@ import { MenuItem } from 'primeng/api';
 })
 export class NavbarComponent implements OnInit {
   items: MenuItem[] = [];
-  nombrePromotora: string = "GLENDY ELIZABETH ZAMBRANO BERMEO";
-
+  nombrePromotora: string = "ELIZABETH ZAMBRANO BERMEO";
+  mensajeWhatsApp = encodeURIComponent('Hola, estoy interesado en obtener información totalmente personalizada y gratis para viajar y trabajar en InCruises.');
+  enlaceWhatsApp = `https://wa.me/+34649926162?text=${this.mensajeWhatsApp}`;
+  enlaceInstagram = `https://www.instagram.com/elzaberh_viaja_barato/`;
+  enlaceTiktok = `https://www.tiktok.com/@elizabehz`;
   ngOnInit() {
     this.items = [
       {
