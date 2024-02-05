615ea3abc4c1cc2d5686f247c74f622cbc0e22ad
RonnyAsanza <ronnyasanza@gmail.com>
2024-02-05 11:30:13 +0100

    video

--- src/app/nosotros/nosotros.component.ts
+++ src/app/nosotros/nosotros.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nosotros',
   templateUrl: './nosotros.component.html',
   styleUrls: ['./nosotros.component.css']
 })
-export class NosotrosComponent {
+export class NosotrosComponent implements AfterViewInit {
+  @ViewChild('videoContainer') videoContainer!: ElementRef;
 
+  ngAfterViewInit() {
+    const newContent = `
+    <div class="z-index-0" #videoContainer>
+      <div data-v-19ea6b67="">
+        <video
+          class="w-full"
+          id="bg-video-loop"
+          autoplay
+          preload="auto"
+          muted
+          src="https://raw.githubusercontent.com/Incruises/videos/master/loop-4.mp4?raw=true"
+        ></video>
+      </div>
+    </div>
+    `;
+    this.videoContainer.nativeElement.innerHTML = newContent;
+  }
 }
